a521fde54e355eca105038afe04e9d32996b15a0
wufuheng@gmail.com <wufuheng@gmail.com>
2013-07-18 13:47:33 +0800

    add heap

--- Algo/bst.h
+++ Algo/bst.h
@@ -5,6 +5,17 @@
 
 using namespace std;
 
+enum WALKORDER{
+	PREORDER=1, /// head->left->right
+	INORDER,    /// left->head->right
+	POSTORDER,  /// left->right->head
+	REVERSE_PREORDER, /// right->left->head
+	REVERSE_INORDER,  /// right->head->left
+	REVERSE_POSTORDER,/// head->right->left
+	LAYERBYLAYER, //layer by layer
+	ZIGZAG,
+	NONE=-1 //
+};
 
 ///@brief arbitrary binary tree node[bst,heap,etc]
 struct btnode{
@@ -19,20 +30,34 @@ struct btnode{
 class bt{
 protected:
 	btnode *proot;
+	virtual void insert(int)=0;
 public:
 	bt():proot(NULL){}
 };
 
-enum WALKORDER{
-		PREORDER=1,
-		INORDER,
-		POSTORDER,
-		LAYERBYLAYER, //layer by layer
-		ZIGZAG,
-		NONE=-1 //
-	};
+///@brief A complete binary tree is a binary tree in which
+///every level, except possibly the last, is completely filled, 
+///and all nodes are as far left as possible.
+///is-a binary tree, but implemented in terms of vector
+class bt_complete:public bt{
+protected:
+	vector<int> v;
+	virtual void insert(int){}
+public:
+	bt_complete(){}
+	~bt_complete(){}
+};
+///@brief A full binary tree (sometimes proper binary tree or 
+///2-tree or strictly binary tree) is a tree in which every
+///node other than the leaves has two children.
+///is-a binary tree, but implemented in terms of vector
+class bt_full:public bt{
+	vector<int> v;
+public:
+};
 
 ///@brief an binary search tree
+///BST is-a Binary Tree
 class bst:public bt{
 	vector<int> walk_from_node(btnode* p, WALKORDER wo) const;
 public:
@@ -58,14 +83,42 @@ public:
 	size_t height_max();
 	size_t inbalance();
 
+	vector<int> getdepth(int n);
+
 	static bool isBST(const bst& t);
 	static WALKORDER getOrder(const vector<int>& v);
 	static bool test();
 
 };
 
+///@brief read black tree is a binary search tree
+class RBbst:public bst{};
 
+///@brief heap is implemented in terms of a binary tree - private inheritance
+class heap:private bt_complete{
+public:
+	virtual void insert(int);
 
+	inline int PARENTPOS(int i){return (i/2);}
+	inline int LCHILDPOS(int i){return (i*2+1);}
+	inline int RCHILDPOS(int i){return (i*2+2);}
+
+	void print();
+
+	static bool test();
+};
+
+//////////////////////////////////////////////////////////////////////////
+namespace sandbox{
+	///@brief heap is implemented in terms of a binary tree - composition
+	template<class T>
+	class heap{
+		bt_complete btc;
+	public:
+		void insert(const T&);
+	};
+
+}
 
 
 

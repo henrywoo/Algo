71b6e98b790f0fc037737217e4004cfdc147732b
wufuheng@gmail.com <wufuheng@gmail.com>
2013-11-07 21:18:10 +0800

    correctness

--- algo11/robot.h
+++ algo11/robot.h
@@ -12,23 +12,30 @@
 #include <windows.h>
 #include <bitset>
 #include <functional>
+#include <chrono>
 
 using namespace std;
 typedef unsigned int uint;
 #define MAXLINE 51*1000*1000
 #define COUNTINGSORTMAXNUM 1<<30
 
+using namespace chrono;
 struct timer{
-  int start;
+  time_point<system_clock> start;
   const char* str;
-  timer(const char* s = "") :start(clock()), str(s){}
-  ~timer(){ printf("[%s] Time Cost: %.3lf\n", str, uint(clock() - start) / CLOCKS_PER_SEC); }
+  timer(const char* s = "") :start(chrono::high_resolution_clock::now()), str(s){}
+  ~timer(){
+    //printf("[%s] Time Cost: %.3lf\n", str, uint(clock() - start) / CLOCKS_PER_SEC);
+    cout << str << ": " << duration_cast<milliseconds>(high_resolution_clock::now() - start).count()
+    //cout << str << ": " << duration_cast<nanoseconds>(high_resolution_clock::now() - start).count()
+      << " milliseconds\n" << endl;
+  }
 };
 
 typedef struct bitarray{
-  bitarray(uint sz){
-    if (sz>0){
-      p = new char[sz / 8 + 1]();
+  bitarray(uint range){
+    if (range>0){
+      p = new char[range / CHAR_BIT + 1]();
     }
   }
   ~bitarray(){
@@ -49,102 +56,101 @@ typedef struct bitarray{
   }
 } barray;
 
-// can be optimized for integer less than 1073741824(2**30)
-// return size of the bucket
-// 1,3,5,10,9,1
-
-void RadixSort(uint* begin, uint* end) {
-  std::vector<uint> bitQueues[2];
-
-  uint bufsz = end - begin;
-  uint sz1 =0, sz2 = 0;
-  for (size_t bit = 0; bit < CHAR_BIT * sizeof(uint); ++bit) {
-    for (uint* itr = begin; itr != end; ++itr) {
-      bitQueues[(*itr & (1u << bit)) ? 1 : 0].push_back(*itr);
-    }
-    if (bit == CHAR_BIT * sizeof(uint)-1)
-      bitQueues[0].swap(bitQueues[1]);
-    if (sz1 = bitQueues[0].size())
-      memcpy_s(begin, bufsz*sizeof(uint), &bitQueues[0][0], sz1*sizeof(uint));
-    if (sz2 = bitQueues[1].size())
-      memcpy_s(begin + sz1, (bufsz - sz1)*sizeof(uint),&bitQueues[1][0], sz2*sizeof(uint));
-    bitQueues[0].clear();
-    bitQueues[1].clear();
-  }
-}
-
-uint getmean(uint *head, size_t sz){
-  timer t(__FUNCSIG__);
+uint AnalysizeData(uint *head, size_t sz,uint& mi, uint& ma){
+  //timer t(__FUNCSIG__);
   uint *end = head + sz;
   uint *tmp = head;
-  int i = 1;
-  double mean = *head;
-  while (++head < end){
-    mean = mean * i / (i + 1) + *head / (i + 1.0);
-    i++;
+  int i = 0;
+  long double mean = 0;
+  while (head < end){
+    if (mi > *head)mi = *head;
+    if (ma < *head)ma = *head;
+    mean = mean * i / (i + 1.f) + *head / (i + 1.0f);
+    ++i;
+    ++head;
   }
   return (uint)mean;
 }
 
-
-uint* mypartition(uint* head, uint* tail, uint pivot){
-  //copy(head,tail+1,ostream_iterator<int>(cout," "));cout << endl;
-  //int pivot = *tail;
-
-  uint* L = head;// the left of L is less than pivot
-  uint* R = tail;// the right of R is greater than pivot
-OUTTER:while (L<R){
-  if (*L>pivot){
-    //waiting for right node to find a node to swap
-    while (L < R){
-      if (*R < pivot){
-        swap(*L, *R);
-        //copy(head,tail+1,ostream_iterator<int>(cout," "));cout << endl;
-        R--;
-        goto INNER;
-      }
-      R--;
+bool verify1(uint* head, uint* tail){
+  for (uint i = 0; i < tail - head;i++){
+    if (head[i] >= head[i + 1]){
+      cout << head[i] << " " << head[i + 1] << endl;
+      return false;
     }
-    goto OUTTER;// cannot find pair to swap, waiting is ended and R==L right now!
   }
-INNER:
-  L++;
-}
-       copy(head,tail+1,ostream_iterator<int>(cout," "));cout << endl;
-       if (*L > pivot){
-         swap(*tail, *L);
-       }
-       else if (pivot == *L){
-       }
-       else{
-         L++;
-         swap(*tail, *L);
-       }
-       //copy(head,tail+1,ostream_iterator<int>(cout," "));cout << endl;
-       return L;
+  return true;
 }
 
 /// radix sort 2.806s
-int AdaptiveSort(uint* head, uint* tail, uint mi, uint ma) {
-  uint m=getmean(head, tail - head + 1);
-  uint hsize = tail - head + 1;
-  uint maximum = ma - mi - 1;
-  if (maximum > (1 << 20)){
-    //mypartition(head, tail, 16);
-    partition(head, tail + 1, bind2nd(less<uint>(),m));
-    //copy(head, tail + 1, ostream_iterator<int>(cout, " ")); cout << endl;
+// return size of the bucket
+// 1,3,5,10,9,1
+int AdaptiveSort(uint* head, uint* tail) {
+  if (head==tail){
+    return 1;
   }
+  uint mi=UINT_MAX, ma=0;
+  uint m=AnalysizeData(head, tail - head + 1,mi,ma);
+  uint hsize = tail - head + 1;
   
-  
-
-  timer t(__FUNCSIG__);
-  if (maximum > (1<<20)){//65536
-    RadixSort(head, tail+1);
-    //sort(head, tail + 1);//0.891
-    //copy(head, tail+1, ostream_iterator<int>(cout, " ")); cout << endl;
-    return unique(head, tail+1)-head+1;
+  uint range = ma - mi - 1;
+  cout << "hsize:" << hsize << " span:" << range << endl;
+  if (range > (1 << 28)){
+    if (hsize < (1 << 22)){
+      timer td("std sort");
+      //std::sort
+      std::sort(head, tail + 1);//0.891
+      //copy(head, tail+1, ostream_iterator<int>(cout, " ")); cout << endl;
+      return unique(head, tail + 1) - head;
+    }else{
+      //timer t("partition");
+      //mypartition(head, tail, 16);
+      for (int i = 0; i < hsize; ++i){
+        if (head[i] == 133050368){
+          cout << "GG" << endl; break;
+        }
+      }
+      uint* p = partition(head, tail + 1, bind2nd(less<uint>(), m));
+      for (int i = 0; i < hsize; ++i){
+        if (head[i] == 133050368){
+          cout << "GG" << endl; break;
+        }
+      }
+      int siz1 = AdaptiveSort(head, p-1);
+      if (!verify1(head, head + siz1 - 1)){
+        cout << "wrong" << endl;
+      }
+      for (int i = 0; i < siz1; ++i){
+        if (head[i] == 133050368){
+          cout << "GG" << endl; break;
+        }
+      }
+      int siz2 = AdaptiveSort(p, tail);
+      if (!verify1(p, p + siz2 - 1)){
+        cout << "wrong" << endl;
+      }
+      cout << head[siz1 - 1] << "\t" << *p << endl;
+      for (int i = 0; i < siz2; ++i){
+        if (p[i] == 133050368){
+          cout << "GG" << endl; break;
+        }
+      }
+      memmove_s(head + siz1, (hsize - siz1)*sizeof(uint), p, siz2*sizeof(uint));
+      if (!verify1(head, head + siz1 + siz2 - 1)){
+        cout << "wrong" << endl;
+      }
+      for (int i = 0; i < siz1+siz2; ++i){
+        if (head[i] == 133050368){
+          cout << "GG" << endl; break;
+        }
+      }
+      return siz1 + siz2;
+      //copy(head, tail + 1, ostream_iterator<int>(cout, " ")); cout << endl;
+    }
+    
   }else{
-    barray ba(maximum);
+    timer ts("counting sort");
+    barray ba(range);
     for (uint i = 0; i < hsize; ++i){
       if (head[i] - mi > 0){
         ba.set(head[i] - mi - 1);
@@ -152,9 +158,16 @@ int AdaptiveSort(uint* head, uint* tail, uint mi, uint ma) {
     }
     uint j = 0;
     head[j++] = mi;
-    for (uint i = 0; i < maximum; ++i){
+    for (uint i = 0; i < range+CHAR_BIT; ++i){
       if (ba.get(i)){
         head[j++] = i + mi + 1;
+      }
+    }
+    if (!verify1(head, head + j-1))
+      cout << "wrong" << endl;
+    for (int i = 0; i < j; ++i){
+      if (head[i] == 133050368){
+        cout << "GG" << endl; break;
       }
     }
     return j;
@@ -174,7 +187,7 @@ public:
   unordered_map<uint, uint> umi;
 
   Robot(uint* bkt_, uint bktsz_, uint* rdata_, uint rdatasz_) :
-    bkt(bkt_), rdata(rdata_), ptree(NULL){}
+    bkt(bkt_),bktsz(bktsz_), rdata(rdata_),rdatasz(rdatasz_), ptree(NULL){}
 
   ~Robot(){
     if (!ptree) delete[] ptree;
@@ -195,60 +208,6 @@ public:
       i += (i & -(int)i);
     }
   }
-#if 0
-
-  //binary search O(logN) - todo
-  uint __getbucketno(int target, uint hi, uint lo = 0){
-    uint mid=0;
-    uint tmp = hi;
-    while (lo <= hi){
-      mid = lo + (hi - lo) / 2;
-      if (bkt[mid] == target)
-        return mid + 1;
-      else if (bkt[mid] < target)
-        lo = mid + 1;
-      else
-        hi = mid - 1;
-    }
-    return -1;// not found
-  }
-
-  void __debug(){
-#ifdef _DEBUG
-    for (int i = 0; i < bktsz; ++i){
-      printf("%d\n", ptree[i]);
-    }
-    printf( "***********************\n" );
-#endif // DEBUG
-  }
-
-  ///@return isboundrypoint
-  bool __getbucketno(uint target, uint& n, uint hi, uint lo){
-    uint mid = 0;
-    while (lo < hi){
-      if (target > bkt[hi] || target < bkt[lo]){
-        n = UINT_MAX;
-        return false;
-      }else if (lo + 1 == hi && bkt[lo] < target && target < bkt[hi]){
-        n = lo + 1;
-        return false;
-      }
-      mid = lo + (hi - lo) / 2;
-      if (bkt[mid] == target){
-        n = mid + 1;
-        return true;
-      }else if (bkt[hi] == target){
-        n = hi + 1;
-        return true;
-      }
-      else if (bkt[mid] < target)
-        lo = mid;
-      else
-        hi = mid;
-    }
-    throw 1;
-  }
-#endif
 
   ///@return num == UINT_MAX means "not in any bucket"
   ///@return isboundrypoint

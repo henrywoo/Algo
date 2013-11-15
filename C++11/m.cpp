4f5550f9a4520daae930efc2b3c025be4762bd21
wufuheng@gmail.com <wufuheng@gmail.com>
2013-11-15 11:04:28 +0800

    20131115

--- C++11/m.cpp
+++ C++11/m.cpp
@@ -9,6 +9,7 @@
 #include <unordered_set>
 #include <iostream>
 #include "multi_producer_consumer.h"
+#include <iomanip>
 
 using namespace std;
 struct Complex final{
@@ -31,8 +32,123 @@ struct Complex final{
   }
 };
 
+//using namespace boost;
+template<typename T>
+class Foo{
+public:
+  //Foo(){}
+  Foo(T){}
+};
+class Foo2 : public Foo<string>{
+public:
+  //Foo2(){}
+};
+//Foo2 f;
+namespace{ static int an = 8; }
+namespace A{
+  namespace{ static int an2=7; }
+}
+using namespace A;
+//------------------------------------------
+template<typename T> class DA{
+  T* c;
+  int sz;
+public:
+  explicit DA(int){}
+};
+DA<int> a1(7);
+DA<int> a2('7');//def contor
+//DA<int> a3=7; // constructor
+DA<int> a4(a1);//cpycotor
+DA<int> a5=a2;//cpycotor
+//------------------------------------------
+#define PPP cout << __FUNCSIG__ << endl;
+namespace{
+  struct A{
+    A(){ PPP }
+    //A(const A&) = delete;
+    virtual void f(){ PPP }
+  };
+  struct B: public A{
+    B(){PPP}
+    virtual void f(){ PPP }
+  };
+  void e1(){
+    try{
+      throw B();
+    }catch (A ex){
+      ex.f();
+    }catch (B& ex){
+      ex.f();
+    }
+  }
+}
+//------------------------------------------
+namespace{
+  bool hasintersection(vector<int>& v1, vector<int>& v2){
+    auto a=find_first_of(v1.begin(), v1.end(), v2.begin(), v2.end());
+    if (a != v1.end()){
+      cout << *a << endl;
+      return true;
+    }else{
+      return false;
+    }
+  }
+  void e2(){
+    vector<int> v1 = { 1, 2, 3 };
+    vector<int> v2 = { 11, 12, 2 };
+    cout << hasintersection(v1, v2) << endl;
+  }
+}
+//------------------------------------------
+namespace ns1{
+  class A{
+    int n;
+    void virtual f() throw(int, double, long){}
+  };
+  struct B: public A{
+    float n;
+    //int virtual f() throw(double, int, long){}
+    void virtual f() throw(double, int, long){
+      PPP
+    }
+    //void f() throw(string){}
+    //void f(){}
+  };
+
+  void e3(){
+    B b;
+    b.f();
+  }
+}
+//------------------------------------------
+namespace ns2{
+  void t(){
+    vector<string> vs(1,"|");
+    fill_n(back_inserter(vs), 5, "h");
+    fill_n(inserter(vs,vs.begin()), 5, "e");
+    //generate_n(vs.begin(), 5, "h");
+  }
+}
+//////////////////////////////////////////////////////////////////////////
+namespace ns3{
+  struct A{ void n(){} void n2() const{} };
+  void test(){
+    const A a;
+    //a.n();// const object cannot call non-const function!!
+    a.n2();
+  }
+}
+//////////////////////////////////////////////////////////////////////////
 
 int main(int argc, char* argv[]){
+  ns2::t();
+  ns1::e3();
+  cout << hex << 18 << setbase(10) << 18<< endl;
+  cout << hex << showbase << 18 << endl;
+  e1();
+  e2();
+  cout << an << endl;
   mpc::test();
   char* d = R"(
 string

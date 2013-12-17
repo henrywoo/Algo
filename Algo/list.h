e0bf9ccb05356b236c52d8c12280d2fd1d8555ad
wufuheng@gmail.com <wufuheng@gmail.com>
2013-12-17 10:48:35 +0800

    sort linked list with quick sort

--- Algo/list.h
+++ Algo/list.h
@@ -3,6 +3,8 @@
 
 #include <vector>
 #include <iostream>
+#include <algorithm> //std::sort
+#include <functional>   // std::greater
 using namespace std;
 
 /**@remarks
@@ -17,199 +19,201 @@ using namespace std;
 // circular的好处.不用维护两个pointer:head,tail.一个就够了.
 
 struct dlist_node{
-	int d;
-	dlist_node* next;
-	dlist_node* prev;
-	dlist_node(int n):d(n){}
+  int d;
+  dlist_node* next;
+  dlist_node* prev;
+  dlist_node(int n) :d(n){}
 };
 
 ///@brief A HEAD_TAIL Double Linked List
 class dlist{
-	dlist_node* psentinel;// no need for front end
+  dlist_node* psentinel;// no need for front end
 
-	size_t lsize;
+  size_t lsize;
 
-	void initialize();
+  void initialize();
 public:
 
-	dlist();
+  dlist();
 
-	dlist(int* p,int* q);
-	dlist(int* p,int size);
-	dlist(const vector<int>& v);
+  dlist(int* p, int* q);
+  dlist(int* p, int size);
+  dlist(const vector<int>& v);
 
-	~dlist();
+  ~dlist();
 
-	/// @brief pass the end, which is the same as the sentinel in this case
-	dlist_node* end();
-	dlist_node* begin();
+  /// @brief pass the end, which is the same as the sentinel in this case
+  dlist_node* end();
+  dlist_node* begin();
 
-	size_t size();
+  size_t size();
 
-	///@brief push new node as the next of sentinel
-	void push_front(int n);
-	void push_front(int* p,int* q);
+  ///@brief push new node as the next of sentinel
+  void push_front(int n);
+  void push_front(int* p, int* q);
 
 
-	/// @brief push new node as the previous of sentinel\n
-	/// A.next=B then B.prev=A, they always appear as a pair
-	void push_end(int n);
+  /// @brief push new node as the previous of sentinel\n
+  /// A.next=B then B.prev=A, they always appear as a pair
+  void push_end(int n);
 
-	/// @brief swap pointer prev and next for every node including sentinel
-	void reverse();
-	static void test();
+  /// @brief swap pointer prev and next for every node including sentinel
+  void reverse();
+  static void test();
 };
 
 ///@brief A Circular Double Linked List
 class dclist{
-	dlist_node* psentinel;// no need for front end
+  dlist_node* psentinel;// no need for front end
 
-	size_t lsize;
+  size_t lsize;
 
-	void initialize();
+  void initialize();
 public:
 
-	dclist();
+  dclist();
 
-	dclist(int* p,int* q);
-	dclist(int* p,int size);
-	dclist(const vector<int>& v);
+  dclist(int* p, int* q);
+  dclist(int* p, int size);
+  dclist(const vector<int>& v);
 
-	~dclist();
+  ~dclist();
 
-	/// @brief pass the end, which is the same as the sentinel in this case
-	dlist_node* end();
-	dlist_node* begin();
+  /// @brief pass the end, which is the same as the sentinel in this case
+  dlist_node* end();
+  dlist_node* begin();
 
-	size_t size();
+  size_t size();
 
-	///@brief push new node as the next of sentinel
-	void push_front(int n);
-	void push_front(int* p,int* q);
+  ///@brief push new node as the next of sentinel
+  void push_front(int n);
+  void push_front(int* p, int* q);
 
 
-	/// @brief push new node as the previous of sentinel\n
-	/// A.next=B then B.prev=A, they always appear as a pair
-	void push_end(int n);
+  /// @brief push new node as the previous of sentinel\n
+  /// A.next=B then B.prev=A, they always appear as a pair
+  void push_end(int n);
 
-	/// @brief swap pointer prev and next for every node including sentinel
-	void reverse();
-	static bool test();
+  /// @brief swap pointer prev and next for every node including sentinel
+  void reverse();
+  static bool test();
 
-    void remove1(int n);
-    void removeall(int n);
+  void remove1(int n);
+  void removeall(int n);
 };
 
 struct slist_node{
-	int d;
-	slist_node* next;
-	slist_node(int n):d(n),next(NULL){}
+  int d;
+  slist_node* next;
+  slist_node(int n) :d(n), next(NULL){}
 };
+
+using COMP = bool(*)(int, int);
+void SortSList(slist_node* sn);
 
 ///@brief A Single Liked List with a head only
 class slist
 {
 public:
-	slist();
-	slist(int* p,int* q);
-	slist(int* p,int size);
-	slist(const vector<int>& v);
-	~slist();
+  slist();
+  slist(int* p, int* q);
+  slist(int* p, int size);
+  slist(const vector<int>& v);
+  ~slist();
 
-	///@brief push new node as the next of sentinel
-	void push(int n);
-	void push(int* p,int* q);
+  ///@brief push new node as the next of sentinel
+  void push(int n);
+  void push(int* p, int* q);
 
-	static bool test();
+  slist_node* end();
 
-	slist_node* end();
+  ///@deprecated no tail for slist
+  slist_node* begin();
 
-	///@deprecated no tail for slist
-	slist_node* begin();
+  ///@brief find the kth node before the tail
+  ///@param k - the kth node before the tail, should be positive
+  ///@return if k is negative, return NULL;\n
+  ///        if k+1 is greater than the length of the list, return NULL;\n
+  ///        if exception occurs, for instance, corrupted list node, return NULL\n
+  ///        otherwise return a pointer of the kth node before the tail node\n
+  ///\note The 0th node is the tail itself\n
+  ///@exception NA
+  ///@throws NA
+  ///@author Wu Fuheng (simonwoo2000@gmail.com)
+  ///\date 25 July, 2013
+  ///@test k=0,-1,1,1000000
+  ///@version 1.0
+  ///@remarks Two pointer tick, like a sliding window
+  ///Time - O(n) where n is the length of the list; Space - O(1)
+  const slist_node* __cdecl FindKthToTail(int k) const throw();
 
-	///@brief find the kth node before the tail
-	///@param k - the kth node before the tail, should be positive
-	///@return if k is negative, return NULL;\n
-	///        if k+1 is greater than the length of the list, return NULL;\n
-	///        if exception occurs, for instance, corrupted list node, return NULL\n
-	///        otherwise return a pointer of the kth node before the tail node\n
-	///\note The 0th node is the tail itself\n
-	///@exception NA
-	///@throws NA
-	///@author Wu Fuheng (simonwoo2000@gmail.com)
-	///\date 25 July, 2013
-	///@test k=0,-1,1,1000000
-	///@version 1.0
-	///@remarks Two pointer tick, like a sliding window
-	///Time - O(n) where n is the length of the list; Space - O(1)
-	const slist_node* __cdecl FindKthToTail(int k) const throw();
+  ///@brief if there is a circle inside the single list
+  bool HasCircle() const;
 
-  static void swap(slist_node* sn1, slist_node* sn2) throw();
-
-	///@brief if there is a circle inside the single list
-	bool HasCircle() const;
-
-    ///@note 3 temporary variables!!!
+  ///@note 3 temporary variables!!!
   void reverse(){// h->a->b->c->NULL
-        slist_node* tmp=head;
-        slist_node* n=tmp->next;
-        slist_node* p=nullptr;
+    slist_node* tmp = head;
+    slist_node* n = tmp->next;
+    slist_node* p = nullptr;
 
-        while(tmp){
-            tmp->next=p;//保存next
-            p=tmp;
-            tmp=n;
-            if(tmp){///EP(error prone)
-                n=tmp->next;//a->next=h
-            }
-        }
+    while (tmp){
+      tmp->next = p;//保存next
+      p = tmp;
+      tmp = n;
+      if (tmp){///EP(error prone)
+        n = tmp->next;//a->next=h
+      }
+    }
   }
 
   void printall(){
-        slist_node* tmp=head;
-        while (tmp!=NULL){
-            cout << tmp->d << endl;
-            tmp= tmp->next;
-        }
+    slist_node* tmp = head;
+    while (tmp != NULL){
+      cout << tmp->d << endl;
+      tmp = tmp->next;
+    }
   }
 
-    void printall_reversely(){
-        slist_node* tmp=head;
-        prhelper(tmp);
-    }
-    void prhelper(slist_node* sn){
-        if (sn){
-            slist_node* tmp=sn->next;
-            prhelper(tmp);
-            //cout << tmp->d << endl;
-            cout << sn->d << endl;
-        }
-        
-    }
+  void printall_reversely(){
+    slist_node* tmp = head;
+    prhelper(tmp);
+  }
 
+  void prhelper(slist_node* sn){
+    if (sn){
+      slist_node* tmp = sn->next;
+      prhelper(tmp);
+      //cout << tmp->d << endl;
+      cout << sn->d << endl;
+    }
+  }
+
+  void sort(COMP pred = [](int a, int b){return a > b; });
+
+  static bool test();
 
 private:
-	slist_node* head;
-	size_t lsize;
+  slist_node* head;
+  size_t lsize;
 };
 
 ///@brief A Circular Single Liked List with a head only
 class sclist
 {
 public:
-	sclist();
-	sclist(int* p,int* q);
-	sclist(int* p,int size);
-	sclist(const vector<int>& v);
-	~sclist();
+  sclist();
+  sclist(int* p, int* q);
+  sclist(int* p, int size);
+  sclist(const vector<int>& v);
+  ~sclist();
 
-	///@brief push new node as the next of sentinel
-	void push_front(int n);
-	void push_front(int* p,int* q);
+  ///@brief push new node as the next of sentinel
+  void push_front(int n);
+  void push_front(int* p, int* q);
 
 private:
-	slist_node* sentinel;
-	size_t lsize;
+  slist_node* sentinel;
+  size_t lsize;
 };
 
 
--- Algo/slist.cpp
+++ Algo/slist.cpp
@@ -1,115 +1,196 @@
 #include "list.h"
 #include <iostream>
 
-slist::slist():head(NULL){}
+slist::slist() :head(NULL){}
 
-slist::slist(int* p,int* q):head(NULL){
-	while (p!=q){
-		push(*p++);
-	}
+slist::slist(int* p, int* q) : head(NULL){
+  while (p != q){
+    push(*p++);
+  }
 }
-slist::slist(int* p,int size):head(NULL){
-	while (size-->=0){
-		push(*p++);
-	}
+slist::slist(int* p, int size) : head(NULL){
+  while (size-- >= 0){
+    push(*p++);
+  }
 }
 slist::slist(const vector<int>& v){}
 
 slist::~slist(){
-	slist_node* tmp=NULL;
-	while(head->next!=NULL){
-		tmp=head;
-		head=head->next;
-		delete tmp;
-	}
+  slist_node* tmp = NULL;
+  while (head->next != NULL){
+    tmp = head;
+    head = head->next;
+    delete tmp;
+  }
 }
 
 // the original head was pushed to the rear
 void slist::push(int n){
-	if (!head){
-		head=new slist_node(n);
-		head->next=NULL;
-	}else{
-		slist_node* tmp=head;
-		head=new slist_node(n);
-		head->next=tmp;
-	}
-	this->lsize++;
+  if (!head){
+    head = new slist_node(n);
+    head->next = NULL;
+  }
+  else{
+    slist_node* tmp = head;
+    head = new slist_node(n);
+    head->next = tmp;
+  }
+  this->lsize++;
 }
 
-void slist::push(int* p,int* q){}
+void slist::push(int* p, int* q){}
 
 slist_node* slist::end(){
-	//return tail->next;
-	return NULL;
+  //return tail->next;
+  return NULL;
 }
 
 slist_node* slist::begin(){
-	return head;
+  return head;
 }
 
 const slist_node* __cdecl slist::FindKthToTail(int k) const throw(){
-	try{
-		if(k<0){
-			return NULL;
-		}else if(k==0){/// just return the tailnode itself
-			slist_node* tmp=head;
-			slist_node* prevtmp=NULL;/// the prev of tmp
-			while (tmp!=NULL){
-				prevtmp=tmp;
-				tmp=tmp->next;
-			}
-			return prevtmp;
-		}else{
-			slist_node* tmp1=head;
-			slist_node* tmp2=head;
-			unsigned int count=0;/// the size of the slist
-			while(tmp1!=NULL){
-				tmp1=tmp1->next;
-				if (count++ >k){
-					tmp2=tmp2->next;
-				}
-			}
-			if (count < k+1){/// no move for tmp2, which means K+1 > length of the link
-				return NULL;
-			}else{
-				return tmp2;
-			}
-		}
-	}catch(...){return NULL;}
+  try{
+    if (k < 0){
+      return NULL;
+    }
+    else if (k == 0){/// just return the tailnode itself
+      slist_node* tmp = head;
+      slist_node* prevtmp = NULL;/// the prev of tmp
+      while (tmp != NULL){
+        prevtmp = tmp;
+        tmp = tmp->next;
+      }
+      return prevtmp;
+    }
+    else{
+      slist_node* tmp1 = head;
+      slist_node* tmp2 = head;
+      unsigned int count = 0;/// the size of the slist
+      while (tmp1 != NULL){
+        tmp1 = tmp1->next;
+        if (count++ > k){
+          tmp2 = tmp2->next;
+        }
+      }
+      if (count < k + 1){/// no move for tmp2, which means K+1 > length of the link
+        return NULL;
+      }
+      else{
+        return tmp2;
+      }
+    }
+  }
+  catch (...){ return NULL; }
 }
 
-void slist::swap(slist_node* sn1, slist_node* sn2){
-    ///@todo
-    slist_node* tmp=sn1;
-
+// swap node is equivalent to swap data
+void SwapNode(slist_node* sn1, slist_node* sn2){
+  ::swap(sn1->d, sn2->d);
 }
 
+slist_node* tailsnode(slist_node* h){
+  slist_node* tmp = h;
+  while (tmp->next){
+    tmp = tmp->next;
+  }
+  return tmp;
+}
+
+slist_node* PartitionSList(slist_node* n1, slist_node* n2, COMP pred)
+{
+  slist_node* pivot = n1;
+  int pdata = pivot->d;
+  slist_node* pBBhead = nullptr, *pBBtail = nullptr; ////Error Prone
+  slist_node* tmp = n1->next;
+  slist_node* psmalltail = nullptr;
+  while (tmp){
+    /*if ((tmp->d) > pdata){*/
+    if (pred(tmp->d, pdata)){
+      if (pBBhead == pBBtail && pBBtail == nullptr){
+        pBBhead = pBBtail = tmp;
+      }
+      else{
+        pBBtail = tmp;
+      }
+    }
+    else{
+      if (pBBhead){
+        SwapNode(pBBhead, tmp);
+        psmalltail = pBBhead;
+        pBBhead = pBBhead->next;
+        pBBtail = tmp;
+      }
+      else{
+        psmalltail = tmp;
+      }
+    }
+    tmp = tmp->next;
+  }
+  if (psmalltail){
+    SwapNode(pivot, psmalltail);
+  }else{
+    psmalltail = pivot;
+  }
+  return psmalltail;/// return the pivot
+}
+
+void qsortSList(slist_node* h, slist_node* t, COMP pred){
+  if (h == NULL || t == NULL || h==t){ return; }
+  slist_node* p = PartitionSList(h, t, pred);
+  qsortSList(h, p, pred);
+  if (p){
+    qsortSList(p->next, t, pred);
+  }
+}
+
+void SortSList(slist_node* sn, COMP pred){
+  qsortSList(sn, tailsnode(sn), pred);
+}
+
+void slist::sort(COMP pred){
+  SortSList(head,pred);
+}
 
 bool slist::test(){
-	int a[]={10,5,7,6,40,25,50,13,21,16,19,9,23,8};
-	slist sl(a,a+(sizeof(a)/sizeof(int)>>1));
-    sl.printall_reversely();
-	int j=(sizeof(a)/sizeof(int)-1);
-	for (int i=-1;i<=j;++i){// test case 1
-		const slist_node* p=sl.FindKthToTail(i);
-		cout << hex << p << "\t" << (p?p->d:0) << endl;
-	}
+  int a[] = { 10, 5, 7, 6, 40, 25, 50, 13, 21, 16, 19, 9, 23, 8 };
+  slist sl(a, a+_countof(a));
+  sl.printall_reversely();
+  int j = (sizeof(a) / sizeof(int)-1);
+  for (int i = -1; i <= j; ++i){// test case 1
+    const slist_node* p = sl.FindKthToTail(i);
+    cout << hex << p << "\t" << (p ? p->d : 0) << endl;
+  }
 
 #if 0
-    for(slist_node* i=sl.begin();i!=sl.end();i=i->next){
-		cout << i->d << endl;
-	}
+  for (slist_node* i = sl.begin(); i != sl.end(); i = i->next){
+    cout << i->d << endl;
+  }
 #endif
 
-	// The paradigm of loop a slist
-	slist_node* i=sl.begin();
-	slist_node* prevnode=NULL;// can be assigned to i, it doesnt matter
-	while(i!=NULL){
-		prevnode=i;
-		i=i->next;
-	}
-	cout << "Last node:" << prevnode->d << endl;
-	return true;
+  // The paradigm of loop a slist
+  slist_node* i = sl.begin();
+  slist_node* prevnode = NULL;// can be assigned to i, it doesnt matter
+  while (i != NULL){
+    prevnode = i;
+    i = i->next;
+  }
+  cout << dec << "Last node:" << prevnode->d << endl;
+
+  sl.sort();
+  sl.printall();
+
+  sl.sort();
+  sl.printall();
+
+  sl.sort([](int a, int b){return a < b; });
+  sl.printall();
+  
+  std::sort(a, a + _countof(a), std::greater<int>());
+  slist sl2(a, a + _countof(a));
+  sl2.sort();
+  sl2.printall();
+
+  return true;
 }
 
